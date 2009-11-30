2725c15731bbf0e36a936e03668923e46a9a36e1
Code_Aster <code-aster at edf fr>
2009-12-01 00:00:00 +0100

    [repo] update to 10.0.29

--- bibpyt/Accas/properties.py
+++ bibpyt/Accas/properties.py
@@ -1,4 +1,4 @@
-#@ MODIF properties Accas DATE 27/11/2009 AUTEUR aster M.ADMINISTRATEUR
+#@ MODIF properties Accas DATE 01/12/2009 AUTEUR aster M.ADMINISTRATEUR
 #            CONFIGURATION MANAGEMENT OF EDF VERSION
 # RESPONSABLE D6BHHHH J-P.LEFEBVRE
 # ======================================================================
@@ -20,6 +20,6 @@
 #     IDENTIFICATION DU GESTIONNAIRE DE COMMANDE ACCAS A PARTIR
 #     DE LA VERSION DU CODE_ASTER ASSOCIE
 #----------------------------------------------------------------------
-version = "10.0.28"
-date = "27/11/2009"
+version = "10.0.29"
+date = "01/12/2009"
 exploit = False
