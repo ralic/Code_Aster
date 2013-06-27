bc709e58ca20b21b6d8586835eaf9c2fff761d06
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2013-06-27 12:29:20 +0200

    [#21018] redimensionnement de jmarq dans jjlihd.F90, modification de l'interface

--- bibfor/jeveux/jjlihd.F90
+++ bibfor/jeveux/jjlihd.F90
@@ -1,5 +1,4 @@
-subroutine jjlihd(idts, nbval, lonoi, genri, typei,&
-                  ltypi, ic, ido, idc, jmarq,&
+subroutine jjlihd(idts, nbval, lonoi, genri, typei,ltypi, ic, ido, idc, jmarq,&
                   iadmi, iadyn)
 ! person_in_charge: j-pierre.lefebvre at edf.fr
 ! aslint: disable=C1002
@@ -28,7 +27,7 @@ subroutine jjlihd(idts, nbval, lonoi, genri, typei,&
     include 'asterfort/jjecrs.h'
     include 'asterfort/jjlidy.h'
     include 'asterfort/u2mess.h'
-    integer :: idts, nbval, lonoi, ltypi, ic, ido, idc, iadmi, jmarq(1)
+    integer :: idts, nbval, lonoi, ltypi, ic, ido, idc, iadmi, jmarq(2)
     character(len=*) :: genri, typei
 ! ----------------------------------------------------------------------
 ! RELIT UN SEGMENT DE VALEURS DANS UN FICHIER HDF EN FONCTION DU TYPE
@@ -38,18 +37,18 @@ subroutine jjlihd(idts, nbval, lonoi, genri, typei,&
 ! A LA PLATE-FORME
 ! L'ETAT ET LE STATUT DU SEGMENT DE VALEURS SONT ACTUALISES
 !
-! IN  IDTS  : IDENTIFICATEUR DU DATASET HDF
-! IN  NBVAL  : NOMBRE DE VALEURS DU DATASET
-! IN  LONOI  : LONGEUR EN OCTET DU SEGMENT DE VALEURS
-! IN  GENRI  : GENRE DE L'OBJET
-! IN  TYPEI  : TYPE DE L'OBJET
-! IN  LTYPI  : LONGUEUR DU TYPE
-! IN  ID0    : IDENTIFICATEUR DE L'OBJET JEVEUX
-! IN  IDC    : IDENTIFICATEUR DE COLLECTION JEVEUX
-! IN  IC     : CLASSE DE L'OBJET JEVEUX
-! IN  JMARQ  : ADRESSE DE LA MARQUE JEVEUX
-! OUT IADMI  : ADRESSE JEVEUX DU SEGMENT DE VALEURS
-! OUT IADYN  : ADRESSE DYNAMIQUE DU SEGMENT DE VALEURS
+! IN     idts  : IDENTIFICATEUR DU DATASET HDF
+! IN     nbval  : NOMBRE DE VALEURS DU DATASET
+! IN     lonoi  : LONGEUR EN OCTET DU SEGMENT DE VALEURS
+! IN     genri  : GENRE DE L'OBJET
+! IN     typei  : TYPE DE L'OBJET
+! IN     ltypi  : LONGUEUR DU TYPE
+! IN     id0    : IDENTIFICATEUR DE L'OBJET JEVEUX
+! IN     idc    : IDENTIFICATEUR DE COLLECTION JEVEUX
+! IN     ic     : CLASSE DE L'OBJET JEVEUX
+! IN/OUT jmarq  : ADRESSE DE LA MARQUE JEVEUX
+! OUT    iadmi  : ADRESSE JEVEUX DU SEGMENT DE VALEURS
+! OUT    iadyn  : ADRESSE DYNAMIQUE DU SEGMENT DE VALEURS
 ! ----------------------------------------------------------------------
 !
 ! ----------------------------------------------------------------------
@@ -76,48 +75,36 @@ subroutine jjlihd(idts, nbval, lonoi, genri, typei,&
     lv = 0
     nbv = nbval
     if (typei .eq. 'I') then
-        call jjalls(lonoi, ic, genri, typei, ltypi,&
-                    'INIT', zi, jadr, iadmi, iadyn)
+        call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', zi, jadr, iadmi, iadyn)
     else if (typei .eq. 'S') then
-        call jjalls(lonoi, ic, genri, typei, ltypi,&
-                    'INIT', izi4, jadr, iadmi, iadyn)
+        call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izi4, jadr, iadmi, iadyn)
     else if (typei .eq. 'R') then
-        call jjalls(lonoi, ic, genri, typei, ltypi,&
-                    'INIT', izr, jadr, iadmi, iadyn)
+        call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izr, jadr, iadmi, iadyn)
     else if (typei .eq. 'C') then
-        call jjalls(lonoi, ic, genri, typei, ltypi,&
-                    'INIT', izc, jadr, iadmi, iadyn)
+        call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izc, jadr, iadmi, iadyn)
         nbv = 2*nbval
     else if (typei .eq. 'K') then
         if (ltypi .eq. 8) then
-            call jjalls(lonoi, ic, genri, typei, ltypi,&
-                        'INIT', izk8, jadr, iadmi, iadyn)
+            call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izk8, jadr, iadmi, iadyn)
         else if (ltypi .eq. 16) then
-            call jjalls(lonoi, ic, genri, typei, ltypi,&
-                        'INIT', izk16, jadr, iadmi, iadyn)
+            call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izk16, jadr, iadmi, iadyn)
         else if (ltypi .eq. 24) then
-            call jjalls(lonoi, ic, genri, typei, ltypi,&
-                        'INIT', izk24, jadr, iadmi, iadyn)
+            call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izk24, jadr, iadmi, iadyn)
         else if (ltypi .eq. 32) then
-            call jjalls(lonoi, ic, genri, typei, ltypi,&
-                        'INIT', izk32, jadr, iadmi, iadyn)
+            call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izk32, jadr, iadmi, iadyn)
         else if (ltypi .eq. 80) then
-            call jjalls(lonoi, ic, genri, typei, ltypi,&
-                        'INIT', izk80, jadr, iadmi, iadyn)
+            call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izk80, jadr, iadmi, iadyn)
         endif
     else if (typei .eq. 'L') then
-        call jjalls(lonoi, ic, genri, typei, ltypi,&
-                    'INIT', izl, jadr, iadmi, iadyn)
+        call jjalls(lonoi, ic, genri, typei, ltypi,'INIT', izl, jadr, iadmi, iadyn)
     endif
-    call jjecrs(iadmi, ic, ido, idc, 'E',&
-                jmarq)
+    call jjecrs(iadmi, ic, ido, idc, 'E',jmarq)
     if (typei .eq. 'I') then
         iconv = 1
         iret = hdftsd(idts,typeb,ltypb,lv)
         if (lois .lt. ltypb) then
             lon = nbval*ltypb
-            call jjalls(lon, ic, 'V', typei, lois,&
-                        'INIT', zi, jadr, kadm, kdyn)
+            call jjalls(lon, ic, 'V', typei, lois,'INIT', zi, jadr, kadm, kdyn)
             iszon(jiszon+kadm-1) = istat(2)
             iszon(jiszon+iszon(jiszon+kadm-4)-4) = istat(4)
             svuse = svuse + (iszon(jiszon+kadm-4) - kadm + 4)
