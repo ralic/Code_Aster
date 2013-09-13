d9f269a0bcd33401353d0e9478b6849680734d35
Mathieu Courtois <mathieu.courtois@edf.fr>
2013-09-13 21:56:49 +0200

    [#21442] create utmess, wrapper on u2mesg
    Pointers to character arrays seems not supported by older gfortran.
    This declaration is not supported (at least with gfortran 4.3):
    character(len=:), pointer :: ptrk(:)

---
+++ bibfor/utilifor/utmess.F90
@@ -0,0 +1,102 @@
+subroutine utmess(typ, idmess, nk, valk, sk, &
+                  ni, vali, si, nr, valr, &
+                  sr)
+! ======================================================================
+! COPYRIGHT (C) 1991 - 2013  EDF R&D                  WWW.CODE-ASTER.ORG
+! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
+! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
+! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
+! (AT YOUR OPTION) ANY LATER VERSION.
+!
+! THIS PROGRAM IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT
+! WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF
+! MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. SEE THE GNU
+! GENERAL PUBLIC LICENSE FOR MORE DETAILS.
+!
+! YOU SHOULD HAVE RECEIVED A COPY OF THE GNU GENERAL PUBLIC LICENSE
+! ALONG WITH THIS PROGRAM; IF NOT, WRITE TO EDF R&D CODE_ASTER,
+!   1 AVENUE DU GENERAL DE GAULLE, 92141 CLAMART CEDEX, FRANCE.
+! ======================================================================
+! person_in_charge: mathieu.courtois at edf.fr
+!
+    implicit none
+#include "asterfort/assert.h"
+#include "asterfort/u2mesg.h"
+    character(len=*), intent(in) :: typ
+    character(len=*), intent(in) :: idmess
+    integer, intent(in), optional :: nk
+    character(len=*), intent(in), optional, target :: valk(*)
+    character(len=*), intent(in), optional :: sk
+    integer, intent(in), optional :: ni
+    integer, intent(in), optional, target :: vali(*)
+    integer, intent(in), optional :: si
+    integer, intent(in), optional :: nr
+    real(kind=8), intent(in), optional, target :: valr(*)
+    real(kind=8), intent(in), optional :: sr
+!
+!   working variables
+    integer :: unk, uni, unr
+    character(len=256), target :: uvk(1)
+!   because it is not supported by older versions of gfortran, we use two different
+!   calls to utmess_core
+!    character(len=:), pointer :: ptrk(:)
+    logical :: use_valk
+    integer, target :: uvi(1)
+    integer, pointer :: ptri(:)
+    real(kind=8), target :: uvr(1)
+    real(kind=8), pointer :: ptrr(:)
+!
+!   associate pointers to valk or sk
+    unk = 1
+    uvk(1) = ' '
+    use_valk = .false.
+    if (present(sk) .or. present(valk) .or. present(nk)) then
+        ASSERT(present(sk) .neqv. (present(valk) .and. present(nk)))
+        if (present(nk)) then
+            unk = nk
+            use_valk = .true.
+        else
+            unk = 1
+            uvk(1) = sk
+        endif
+    endif
+!   associate pointers to vali or si
+    uni = 1
+    uvi(1) = 0
+    ptri => uvi(1:1)
+    if (present(si) .or. present(vali) .or. present(ni)) then
+        ASSERT(present(si) .neqv. (present(vali) .and. present(ni)))
+        if (present(ni)) then
+            uni = ni
+            ptri => vali(1:ni)
+        else
+            uni = 1
+            uvi(1) = si
+            ptri => uvi(1:1)
+        endif
+    endif
+!   associate pointers to valr or sr
+    unr = 1
+    uvr(1) = 0.d0
+    ptrr => uvr(1:1)
+    if (present(sr) .or. present(valr) .or. present(nr)) then
+    ASSERT(present(sr) .neqv. (present(valr) .and. present(nr)))
+        if (present(nr)) then
+            unr = nr
+            ptrr => valr(1:nr)
+        else
+            unr = 1
+            uvr(1) = sr
+            ptrr => uvr(1:1)
+        endif
+    endif
+!
+    if (use_valk) then
+        call u2mesg(typ, idmess, unk, valk, uni, &
+                    ptri, unr, ptrr)
+    else
+        call u2mesg(typ, idmess, unk, uvk, uni, &
+                    ptri, unr, ptrr)
+    endif
+!
+end subroutine utmess
