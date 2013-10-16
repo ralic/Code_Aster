654ec7afac11cb1af3b324092192b3e14ea6c4c9
Hassan Berro <hassan.berro at edf.fr>
2013-10-16 16:21:18 +0200

    [#20041] new op CALC_CHAM_FLUI: fluid-structure pressure field

---
+++ bibfor/algorith/chflch.F90
@@ -0,0 +1,215 @@
+subroutine chflch(rigthe, vec2nd)
+    implicit none
+!
+! ======================================================================
+! COPYRIGHT (C) 1991 - 2013  EDF R&D                  WWW.CODE-ASTER.ORG
+! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
+! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
+! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
+! (AT YOUR OPTION) ANY LATER VERSION.
+!
+! THIS PROGRAM IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT
+! WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF
+! MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. SEE THE GNU
+! GENERAL PUBLIC LICENSE FOR MORE DETAILS.
+!
+! YOU SHOULD HAVE RECEIVED A COPY OF THE GNU GENERAL PUBLIC LICENSE
+! ALONG WITH THIS PROGRAM; IF NOT, WRITE TO EDF R&D CODE_ASTER,
+!    1 AVENUE DU GENERAL DE GAULLE, 92141 CLAMART CEDEX, FRANCE.
+! ======================================================================
+! person_in_charge: hassan.berro at edf.fr
+!
+#include "jeveux.h"
+#include "asterc/getfac.h"
+#include "asterfort/asasve.h"
+#include "asterfort/ascavc.h"
+#include "asterfort/ascova.h"
+#include "asterfort/assert.h"
+#include "asterfort/dismoi.h"
+#include "asterfort/exisd.h"
+#include "asterfort/getvid.h"
+#include "asterfort/jedema.h"
+#include "asterfort/jedetr.h"
+#include "asterfort/jeexin.h"
+#include "asterfort/jelira.h"
+#include "asterfort/jemarq.h"
+#include "asterfort/jeveuo.h"
+#include "asterfort/lxlgut.h"
+#include "asterfort/utmess.h"
+#include "asterfort/vechth.h"
+#include "asterfort/vedith.h"
+#include "asterfort/vtcreb.h"
+#include "asterfort/wkvect.h"
+!   ------------------------------------------------------------------------------------
+!   *** Subroutine arguments
+!   ------------------------------------------------------------------------------------
+!       --> Input variables
+    character(len=8) :: rigthe
+!       <-- Output variables
+    character(len=24) :: vec2nd
+!
+!   ------------------------------------------------------------------------------------
+!   *** Definition of local variables
+!   ------------------------------------------------------------------------------------
+    integer :: neq, nchar, ialich, jinf, ialifc, nchci, ich, iret, jnchtp, j2nd, lonch, k
+    integer :: n1, jpro, jval, loncm1, jndirp
+    real    :: tpsthe(1)
+    logical :: fmult, coecst
+    character(len=8) :: typch, parcha, mate, carele, numedd
+    character(len=19) :: infcha, nomcha
+    character(len=24) :: charge, fomult, vechtp, vechtn, infoch, ligrch, lchin, cnchci, modele
+    character(len=24) :: vachtp, cnchtp, nomfct, vediri, vadirp, cndirp, inst
+!   ------------------------------------------------------------------------------------
+    integer :: nbtych
+    parameter   (nbtych = 11)
+    character(len=6) :: nomlig(nbtych)
+    nomlig=['.CIMPO'  ,'.SOURE'  ,'.FLURE'  ,'.FLUR2'  ,&
+     &     '.T_EXT'  ,'.COEFH'  ,'.HECHP'  ,'.GRAIN'  ,'.FLUNL'  ,&
+     &     '.SOUNL'  , '.RAYO '   ]
+!   ------------------------------------------------------------------------------------
+    vediri ='&&VETDIR           .RELR'
+    vechtp ='&&VETCHA           .RELR'
+    vechtn ='&&VENCHA           .RELR'
+    infcha ='&&OP0116_INF_CHARGE'
+    cndirp ='    '
+    cnchtp ='    '
+    tpsthe(1) = 0.0d0
+    inst= ' '
+
+    call jemarq()
+
+    call dismoi('NOM_MODELE',rigthe, 'MATR_ASSE', repk=modele)
+    call dismoi('CHAM_MATER',rigthe, 'MATR_ASSE', repk=mate)
+    call dismoi('CARA_ELEM',rigthe, 'MATR_ASSE', repk=carele)
+    call dismoi('NOM_NUME_DDL',rigthe, 'MATR_ASSE', repk=numedd)
+
+    call vtcreb(vec2nd, 'V', 'R', nume_ddlz=numedd, nb_equa_outz=neq)
+
+    charge = infcha//'.LCHA'
+    infoch = infcha//'.INFC'
+    fomult = infcha//'.FCHA'
+
+    call getfac('EXCIT', nchar)
+
+    if (nchar .ne. 0) then
+        call wkvect(charge, 'V V K24', nchar, ialich)
+        call wkvect(infoch, 'V V IS', 2*nchar+1, jinf)
+        call wkvect(fomult, 'V V K24', nchar, ialifc)
+        zi(jinf) = nchar
+        nchci = 0
+        do 32 , ich = 1 , nchar
+            call getvid('EXCIT', 'CHARGE', iocc=ich, scal=nomcha)
+            zk24(ialich+ich-1) = nomcha
+
+            call dismoi('TYPE_CHARGE', nomcha, 'CHARGE', repk=typch)
+            if ((typch(1:5).ne.'THER_') .and. (typch(1:5).ne.'CITH_')) then
+                call utmess('E', 'CHARGES_21', sk=nomcha(1:8))
+            endif
+
+            ligrch = nomcha(1:8)//'.CHTH.LIGRE'
+
+            if (typch(1:5) .eq. 'CITH_') then
+                call jeexin(nomcha(1:19)//'.AFCK', iret)
+                ASSERT(iret.ne.0)
+                if (typch(5:7) .eq. '_FT') then
+                    zi(jinf+ich) = -3
+                else if (typch(5:7).eq.'_FO') then
+                    zi(jinf+ich) = -2
+                else
+                    zi(jinf+ich) = -1
+                endif
+            endif
+
+            lchin = ligrch(1:13)//'.CIMPO.DESC'
+            call jeexin(lchin, iret)
+            if (iret .ne. 0) then
+                if (typch(5:7) .eq. '_FO') then
+                    zi(jinf+ich) = 2
+                    call dismoi('PARA_INST', lchin(1:19), 'CARTE', repk=parcha)
+                    if (parcha(1:3) .eq. 'OUI') then
+                        zi(jinf+ich) = 3
+                    endif
+                else
+                    zi(jinf+ich) = 1
+                endif
+            endif
+
+            fmult = .false.
+            call getvid('EXCIT', 'FONC_MULT', iocc=ich, scal=zk24( ialifc+ich-1), nbret=n1)
+
+            if (n1 .eq. 0) then
+                nomfct = '&&OP0116'
+                call jeexin(nomfct(1:19)//'.PROL', iret)
+                if (iret .eq. 0) then
+                    ASSERT(lxlgut(nomfct).le.24)
+                    call wkvect(nomfct(1:19)//'.PROL', 'V V K24', 6, jpro)
+                    zk24(jpro) = 'CONSTANT'
+                    zk24(jpro+1) = 'CONSTANT'
+                    zk24(jpro+2) = 'TOUTPARA'
+                    zk24(jpro+3) = 'TOUTRESU'
+                    zk24(jpro+4) = 'CC      '
+                    zk24(jpro+5) = nomfct
+!    
+                    call wkvect(nomfct(1:19)//'.VALE', 'V V R', 2, jval)
+                    zr(jval) = 1.0d0
+                    zr(jval+1)= 1.0d0
+                endif
+                zk24(ialifc+ich-1) = '&&OP0116'
+            else
+                fmult = .true.
+            endif
+
+            do 326 , k = 2 ,nbtych
+                lchin = ligrch(1:13)//nomlig(k)//'.DESC'
+                call exisd('CHAMP_GD', lchin, iret)
+                if (iret .ne. 0) then
+                    if ((k.ge.7) .and. fmult) then
+                        call utmess('F', 'CHARGES_20', sk=nomcha(1:8))
+                    endif
+                    if (typch(5:7) .eq. '_FO') then
+                        zi(jinf+nchar+ich) = max(2,zi(jinf+nchar+ich))
+                        call dismoi('PARA_INST', lchin(1:19), 'CARTE', repk=parcha)
+                        if (parcha(1:3) .eq. 'OUI') then
+                            if (nomlig(k) .ne. '.T_EXT') then
+                                coecst = .false.
+                            endif
+                            zi(jinf+nchar+ich) = max(3,zi(jinf+nchar+ ich))
+                        endif
+                    else
+                        zi(jinf+nchar+ich) = max(1,zi(jinf+nchar+ich))
+                    endif
+                endif
+326         continue
+32      continue
+    endif
+!
+!   --- Dirichlet
+    call vedith(modele, infcha, inst, vediri)
+    call asasve(vediri, numedd, 'R', vadirp)
+    call ascova('D', vadirp, fomult, 'INST', tpsthe(1),&
+                'R', cndirp)
+    call jeveuo(cndirp(1:19)//'.VALE', 'L', jndirp)
+!   --- Cinematique
+    cnchci = ' '
+    call ascavc(charge, infoch, fomult, numedd, tpsthe(1),&
+                cnchci)
+!   --- Other
+    call vechth('STAT',modele, charge, infoch, carele, mate,&
+                tpsthe(1), ' ', ' ', vechtp)
+    call asasve(vechtp, numedd, 'R', vachtp)
+    call ascova('D', vachtp, fomult, 'INST', tpsthe(1),&
+                'R', cnchtp)
+    call jeveuo(cnchtp(1:19)//'.VALE', 'L', jnchtp)
+    call jedetr(vechtp)
+    call jedetr(vechtn)
+! 
+    call jeveuo(vec2nd(1:19)//'.VALE', 'E', j2nd)
+    call jelira(vec2nd(1:19)//'.VALE', 'LONMAX', lonch)
+    loncm1 = lonch - 1
+    do k = 0, loncm1
+        zr(j2nd+k) = zr(jnchtp+k) + zr(jndirp+k)
+    enddo
+
+    call jedema()
+
+end subroutine
--- bibfor/op/op0116.F90
+++ bibfor/op/op0116.F90
@@ -1,7 +1,6 @@
 subroutine op0116()
     implicit none
 #include "asterfort/utmess.h"
-!     ------------------------------------------------------------------
 ! ======================================================================
 ! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
@@ -18,6 +17,226 @@ subroutine op0116()
 ! ALONG WITH THIS PROGRAM; IF NOT, WRITE TO EDF R&D CODE_ASTER,
 !    1 AVENUE DU GENERAL DE GAULLE, 92141 CLAMART CEDEX, FRANCE.
 ! ======================================================================
-!     ------------------------------------------------------------------
-    call utmess('F', 'FERMETUR_5')
+!
+! person_in_charge: hassan.berro@edf.fr
+! OPERATEUR CALCULANT LE CHAMP DE PRESSION DANS UN FLUIDE SELON LES
+! HYPOTHESES D'UN ECOULEMENT POTENTIEL
+!
+#include "asterf_types.h"
+#include "jeveux.h"
+#include "asterc/r8pi.h"
+#include "asterc/r8prem.h"
+#include "asterc/getres.h"
+#include "asterfort/alimrs.h"
+#include "asterfort/calflu.h"
+#include "asterfort/chpver.h"
+#include "asterfort/chflch.h"
+#include "asterfort/copisd.h"
+#include "asterfort/dismoi.h"
+#include "asterfort/getvid.h"
+#include "asterfort/getvr8.h"
+#include "asterfort/getvtx.h"
+#include "asterfort/jedema.h"
+#include "asterfort/jemarq.h"
+#include "asterfort/jeveuo.h"
+#include "asterfort/ntarch.h"
+#include "asterfort/ntetcr.h"
+#include "asterfort/nxnoli.h"
+#include "asterfort/prstoc.h"
+#include "asterfort/rcmfmc.h"
+#include "asterfort/resoud.h"
+#include "asterfort/rsadpa.h"
+#include "asterfort/rsexch.h"
+#include "asterfort/rsorac.h"
+#include "asterfort/tabcor.h"
+#include "asterfort/vtcmbl.h"
+#include "asterfort/wkvect.h"
+!
+    aster_logical :: ltrans, lbid
+    integer :: i, ib, imod,  nbmod, j1, jdisc, dim, icor(2), iadx, iady 
+    integer :: nbdesc, nbrefe, nbvale, nbcoefs, icoef, jcoef
+    real(kind=8) :: r8b, pi, freq, para(2), constant,  epstol
+    character(len=2) :: modelisa(2), modelis
+    character(len=8) :: mater, rigthe, modmec, nomres, potentiel
+    character(len=8) :: mailla, maflui, nume
+    character(len=19) :: k19b, chamno, vectas, solve1, solve2, resuas, nomch(3)
+    character(len=19) :: tmpmod
+    character(len=24) :: typres, nomcom, mate, metres, sdieto, arcinf, inflis
+    character(len=24) :: chcmb2, vesolx, vesoly, vesolz, vect2
+    character(len=24) :: liditr, nuddl, modele, bl24, chcomb, chamnx, chamny, chamnz
+    complex(kind=8) :: c16b
+    modele  = ' '
+    mater   = ' '
+    mate    = ' '
+    rigthe  = ' '
+    modmec  = ' '
+    chamno  = ' '
+    bl24    = ' '
+    para(1) = 1.0
+    para(2) = -1.d150
+    pi = r8pi()
+    epstol = 1.d5*r8prem()
+    r8b=0.d0
+    c16b = dcmplx(0.,0.)
+!---------------------------------------------------------------------
+    call jemarq()
+!---------------------------------------------------------------------
+    call getres(nomres, typres, nomcom)
+    
+!---------------------------------------------------------------------
+!---------- RECUPERATION DES ARGUMENTS DE LA COMMANDE ----------------
+!---------------------------------------------------------------------
+    call getvtx(' ', 'POTENTIEL' , iocc=1, scal=potentiel, nbret=ib)
+    call getvid(' ', 'RIGI_THER',  iocc=1, scal=rigthe, nbret=ib)
+    call getvid(' ', 'MODE_MECA',  iocc=1, scal=modmec, nbret=imod)
+!---------------------------------------------------------------------
+!---------- MODELE THERMIQUE ET CHAMP DE MATERIAUX -------------------
+!---------------------------------------------------------------------
+    call dismoi('NOM_MODELE', rigthe, 'MATR_ASSE', repk=modele)
+    call dismoi('CHAM_MATER', rigthe, 'MATR_ASSE', repk=mater )
+    if (ib .ne. 0) call rcmfmc(mater, mate)
+!---------------------------------------------------------------------
+!---------- NUMEROTATION DES DDL FLUIDES -----------------------------
+!---------------------------------------------------------------------
+    call dismoi('NOM_NUME_DDL', rigthe, 'MATR_ASSE', repk=nuddl)
+
+!---------------------------------------------------------------------
+!---------- PARAMETRES DU SOLVEUR ------------------------------------
+!---------------------------------------------------------------------
+    call dismoi('SOLVEUR', rigthe, 'MATR_ASSE', repk=solve1)
+    call jeveuo(solve1//'.SLVK', 'E', j1)
+    metres=zk24(j1)
+    if (metres .ne. 'MUMPS' .and. metres .ne. 'PETSC') then
+        solve2='&&OP0116.SOLVEUR'
+        call copisd('SOLVEUR', 'V', solve1, solve2)
+    else
+        solve2=solve1
+    endif
+
+    call dismoi('DIM_GEOM', modele, 'MODELE', repi=dim)
+    call dismoi('NOM_MAILLA', modele, 'MODELE', repk=maflui)
+
+    modelisa = ['2D' , '3D']
+    modelis = modelisa(dim-1)
+
+    vect2 = '&&OP0116.2ND.MEMBRE'
+    call chflch(rigthe,vect2)
+
+
+!---------------------------------------------------------------------
+!---------- APPEL A CALFLU -------------------------------------------
+!---------------------------------------------------------------------
+    if (imod .ne. 0) then
+        call dismoi('NUME_DDL', modmec, 'RESU_DYNA', repk=nume)
+        call dismoi('NOM_MAILLA', nume, 'NUME_DDL', repk=mailla)
+        call dismoi('NB_CHAMPS', modmec, 'RESU_DYNA', repi=nbmod)
+        call wkvect('&&OP0116.TXSTO', 'V V K24', nbmod, iadx)
+        call wkvect('&&OP0116.TYSTO', 'V V K24', nbmod, iady)
+        call wkvect('&&OP0116.TZSTO', 'V V K24', nbmod, iady)
+
+        call tabcor(modelis, mate, mailla, maflui, modele, nuddl, 0, icor)
+
+        call getvr8(' ', 'COEF_MULT',  iocc=1, nbval=0, nbret=icoef)
+        nbcoefs = 1
+        if (icoef .ne. 0) nbcoefs = -icoef
+
+        call wkvect('&&OP0116.COEFSMOD', 'V V R', nbcoefs, jcoef)
+        call getvr8(' ', 'COEF_MULT',  iocc=1, nbval=nbcoefs, vect=zr(jcoef))
+
+        do 10 i = 1, nbmod
+            call rsexch('F', modmec, 'DEPL', i, chamno, ib)
+            call rsadpa(modmec, 'L', 1, 'FREQ', i, 0, sjv=j1)
+            freq = zr(j1)
+!
+            constant = 1
+            if (i .le. nbcoefs) then
+                constant = zr(jcoef+i-1)
+            endif
+!
+            tmpmod = '&&OP0116.MODxMOIN.1'
+            if (potentiel(1:5) .eq. 'PRESS') then
+                constant = constant * 4.d0*pi*pi*freq*freq
+            elseif (potentiel(1:4) .eq. 'VITE') then
+                constant = constant * 2.d0*pi*freq
+            elseif (potentiel(1:4) .eq. 'DEPL') then
+                constant = constant * 1.0
+            endif
+!
+            call vtcmbl(1, 'R', [constant], 'R', [chamno], 'R', tmpmod)
+!
+            chamnx = '&&OP0116.CHAMNX'
+            chamny = '&&OP0116.CHAMNY'
+            vesolx = '&&OP0116.VESOLX'
+            vesoly = '&&OP0116.VESOLY'
+            call alimrs(mate, mailla, maflui, modele, 0,nuddl, tmpmod, chamnx, 'DX', icor)
+            call alimrs(mate, mailla, maflui, modele, 0,nuddl, tmpmod, chamny, 'DY', icor)
+            call calflu(chamnx, modele, mate, nuddl, vesolx, nbdesc, nbrefe, nbvale, 'X')
+            call calflu(chamny, modele, mate, nuddl, vesoly, nbdesc, nbrefe, nbvale, 'Y')
+!
+            chcomb = "&&OP0116.CHCOMB"
+            nomch  = [vect2(1:19), vesolx(1:19),vesoly(1:19)]
+            call vtcmbl(3, ['R','R','R'], [1.d0, 1.d0, 1.d0], ['R','R','R'], nomch,'R', chcomb)
+!
+            if (dim .eq. 3) then
+                chamnz = '&&OP0116.CHAMNZ'
+                vesolz = '&&OP0116.VESOLZ'
+                call alimrs(mate, mailla, maflui, modele, 0,nuddl, tmpmod, chamnz, 'DZ', icor)
+                call calflu(chamnz, modele, mate, nuddl, vesolz, nbdesc, nbrefe, nbvale, 'Z')
+!
+                chcmb2 = "&&OP0116.CHCOMB2"
+                call vtcmbl(2, ['R','R'], [1.d0, 1.d0], ['R','R'], [chcomb,vesolz], 'R', chcmb2)
+                chcomb = chcmb2
+            endif
+!
+            vectas = chcomb(1:19)
+            resuas = '&&NXLECTVAR_____'
+            call chpver('F', vectas, 'NOEU', '*', ib)
+            call resoud(rigthe, ' ', solve2, ' ', 0,&
+                        vectas, resuas, 'V', [r8b], [c16b],&
+                        ' ', .true._1, 0, ib)
+!
+            if (i .eq. 1) then
+                ltrans = .false.
+                k19b = '                   '
+                sdieto = '&&OP0116.SDIETO'
+                call ntetcr(nuddl, .false._1, sdieto)
+!
+                inflis = '&&OP0116.SDDIS.ARCH.INFL'
+                call wkvect(inflis, 'V V R', 3, j1)
+                zr(j1) = 1.0
+                zr(j1+1) = 1.0
+                zr(j1+2) = nbmod
+!
+                arcinf = '&&OP0116.SDDIS.ARCH.AINF'
+                call wkvect(arcinf, 'V V I', nbmod, j1)
+                zi(j1) = 0
+!
+                liditr = '&&OP0116.SDDIS.ARCH.DITR'
+                call wkvect(liditr, 'V V R', nbmod, jdisc)
+                zr(jdisc+1-1) = freq
+!
+                call nxnoli(modele, mate, bl24, .false._1, .false._1,&
+                            .false._1, ltrans, para, arcinf(1:19), k19b,&
+                            sdieto, k19b)
+            endif
+            
+            k19b = '                   '
+!           --- Pour les modes doubles, assurer la qualite monotone de la discretisation
+!               en frequence (indispensable pour un stockage evol_ther)
+            if (i.gt.1) then
+                if (freq.le. zr(jdisc+i-2)) then
+                    freq = zr(jdisc+i-2) + epstol
+                end if
+            end if
+            zr(jdisc+i-1) = freq
+
+            call ntarch(i-1, modele, mate, bl24, .false._1,&
+                        para, arcinf(1:19), k19b, sdieto, k19b,&
+                        lbid)
+!
+10      continue
+    endif
+!
+!
+    call jedema()
 end subroutine
