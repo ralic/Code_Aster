f0a8b29b7d584610cbcfd90183b301a176c7b0b9
Alexandre Foucault <alexandre.foucault@edf.fr>
2013-10-29 15:48:51 +0100

    [#20861]: prise en compte de la dilatation thermique avec BETON_RAG

--- bibfor/lc/lc0044.F90
+++ bibfor/lc/lc0044.F90
@@ -24,6 +24,7 @@ subroutine lc0044(fami, kpg, ksp, ndim, imate,&
     implicit none
 #include "asterfort/brag00.h"
 #include "asterfort/rcvarc.h"
+#include "asterfort/rcvalb.h"
     integer :: imate, ndim, kpg, ksp, codret, icomp, nvi, iret
     real(kind=8) :: crit(*), angmas(*)
     real(kind=8) :: instam, instap, tampon(*)
@@ -35,6 +36,11 @@ subroutine lc0044(fami, kpg, ksp, ndim, imate,&
     character(len=8) :: typmod(*)
     character(len=*) :: fami
 !
+    real(kind=8) :: valres(1),alpham,alphap
+    real(kind=8) :: epsmc(6),depsc(6)
+    character(len=8) :: nomres(1)
+    integer :: i,icodre(2)
+!
 ! APPEL DE RCVARC POUR LE CALCUL DE LA TEMPERATURE
 !
     call rcvarc('F', 'TEMP', '-', fami, kpg,&
@@ -44,11 +50,39 @@ subroutine lc0044(fami, kpg, ksp, ndim, imate,&
     call rcvarc('F', 'TEMP', 'REF', fami, kpg,&
                 ksp, tref, iret)
 !
+! PRISE EN COMPTE DES DEFORMATIONS THERMIQUES
+!
+    nomres(1)='ALPHA'
+    call rcvalb(fami, kpg, ksp, '-', imate,&
+                ' ', 'ELAS', 0, ' ', [0.d0],&
+                1, nomres, valres, icodre, 2)
+    alpham = valres(1)
+    call rcvalb(fami, kpg, ksp, '+', imate,&
+                ' ', 'ELAS', 0, ' ', [0.d0],&
+                1, nomres, valres, icodre, 2)
+    alphap = valres(1)
+    
+    if ((option(1:9) .eq. 'RAPH_MECA') .or. (option(1:9) .eq. 'FULL_MECA')) then
+      do 10 i = 1, 3
+        depsc(i) = deps(i) - (alphap*(tp-tref)-alpham*(tm-tref))
+        epsmc(i) = epsm(i) - alpham*(tm-tref)
+        depsc(i+3) = deps(i+3)
+        epsmc(i+3) = epsm(i+3)
+ 10   continue
+    else
+      do 20 i = 1, 3
+        depsc(i) = 0.d0
+        depsc(i+3) = 0.d0
+        epsmc(i) = epsm(i) - alpham*(tm-tref)
+        epsmc(i+3) = epsm(i+3)
+ 20   continue
+    endif
+!
 ! ------------------------------------------------
 !
 !
     call brag00(fami, kpg, ksp, ndim, typmod,&
                 imate, compor, instam, instap, tm,&
-                tp, tref, epsm, deps, sigm,&
+                tp, tref, epsmc, depsc, sigm,&
                 vim, option, sigp, vip, dsidep)
 end subroutine
