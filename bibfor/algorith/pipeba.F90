e9ffdd942d5125277e6c1ca32e8b928a1e48133d
Mathieu Courtois <mathieu.courtois@edf.fr>
2013-10-31 17:58:12 +0100

    [#21476] add interface for pipeba
    - change copilo(2,3) into copilo(5)

--- bibfor/algorith/pipeba.F90
+++ bibfor/algorith/pipeba.F90
@@ -24,8 +24,13 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
 #include "asterfort/rcvalb.h"
 #include "asterfort/zerop2.h"
 #include "blas/ddot.h"
-    integer :: mate, ndim
-    real(kind=8) :: sup(ndim), sud(ndim), vim, dtau, copilo(2, 3)
+    integer, intent(in) :: ndim
+    integer, intent(in) :: mate
+    real(kind=8), intent(in) :: sup(ndim)
+    real(kind=8), intent(in) :: sud(ndim)
+    real(kind=8), intent(in) :: vim
+    real(kind=8), intent(in) :: dtau
+    real(kind=8), intent(out) :: copilo(5)
 !
 !-----------------------------------------------------------------------
 !
@@ -77,18 +82,18 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
     p0=0.d0
     p1=0.d0
     p2=0.d0
-    do 10 i = 2, ndim
+    do i = 2, ndim
         p2 = p2 + sud(i)*sud(i)
         p1 = p1 + sud(i)*sup(i)
         p0 = p0 + sup(i)*sup(i)
-10  end do
+    end do
 !
 !    PAS DE SOLUTION
-    if (p2 .lt. (1.d0/r8gaem()**0.5d0)) goto 1000
+    if (p2 .lt. (1.d0/r8gaem()**0.5d0)) goto 100
 !
 !    RECHERCHE DES SOLUTIONS
     call zerop2(2*p1/p2, (p0-c**2)/p2, rac, nrac)
-    if (nrac .le. 1) goto 1000
+    if (nrac .le. 1) goto 100
 !
     xn = sup(1)+rac(2)*sud(1)
     if (xn .le. 0 .and. xn .ge. -kref) then
@@ -106,7 +111,7 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
         a0(2) = dtau-eta(2)*a1(2)
     endif
 !
-1000  continue
+100 continue
 !
 !
 !    PORTION EN TRACTION : FEL = (SQR(SU(1)**2 + SU(2)**2) - KA) / KREF
@@ -116,11 +121,11 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
     p0 = ddot(ndim,sup,1,sup,1)
 !
 !    PAS DE SOLUTION
-    if (p2 .lt. (1.d0/r8gaem()**0.5d0)) goto 2000
+    if (p2 .lt. (1.d0/r8gaem()**0.5d0)) goto 200
 !
 !    RECHERCHE DES SOLUTIONS
     call zerop2(2*p1/p2, (p0-c**2)/p2, rac, nrac)
-    if (nrac .le. 1) goto 2000
+    if (nrac .le. 1) goto 200
 !
     if (sup(1)+rac(2)*sud(1) .gt. 0) then
         ok(3) = 1
@@ -136,7 +141,7 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
         a0(4) = dtau-eta(4)*a1(4)
     endif
 !
-2000  continue
+200 continue
 !
 !
 ! -- CLASSEMENT DES SOLUTIONS
@@ -145,14 +150,14 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
     ASSERT(nsol.le.2)
 !
     j = 0
-    do 20 i = 1, 4
+    do i = 1, 4
         if (ok(i) .eq. 1) then
             j = j+1
             etasol(j) = eta(i)
-            copilo(1,j) = a0(i)
-            copilo(2,j) = a1(i)
+            copilo(1 + 2*(j-1)) = a0(i)
+            copilo(2 + 2*(j-1)) = a1(i)
         endif
-20  end do
+    end do
 !
 !    ON RANGE LES SOLUTIONS DANS L'ORDRE CROISSANT (SI NECESSAIRE)
     if (nsol .eq. 2) then
@@ -161,13 +166,13 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
             etasol(2) = etasol(1)
             etasol(1) = tmp
 !
-            tmp = copilo(1,1)
-            copilo(1,1) = copilo(1,2)
-            copilo(1,2) = tmp
+            tmp = copilo(1)
+            copilo(1) = copilo(3)
+            copilo(3) = tmp
 !
-            tmp = copilo(2,1)
-            copilo(2,1) = copilo(2,2)
-            copilo(2,2) = tmp
+            tmp = copilo(2)
+            copilo(2) = copilo(4)
+            copilo(4) = tmp
         endif
     endif
 !
@@ -177,14 +182,14 @@ subroutine pipeba(ndim, mate, sup, sud, vim,&
 !    SI DEPLACEMENT PILOTE NUL ET SAUT EQ INFERIEUR A DTAU
 !    ON IGNORE LE POINT POUR LA RESOLUTION GLOBALE
         if (p2 .le. (1.d0/r8gaem()**0.5d0) .and. (sqrt(p0)) .le. dtau) then
-            copilo(1,1) = 0.d0
-            copilo(1,2) = 0.d0
-            copilo(2,1) = 0.d0
-            copilo(2,2) = 0.d0
+            copilo(1) = 0.d0
+            copilo(2) = 0.d0
+            copilo(3) = 0.d0
+            copilo(4) = 0.d0
 ! DANS LES AUTRE CAS COURBE TJS SUPERIEURE A DTAU : ON PLANTE
         else
-            copilo(1,1) = 1.d0
-            copilo(1,3) = 1.d0
+            copilo(1) = 1.d0
+            copilo(5) = 1.d0
         endif
     endif
 !
