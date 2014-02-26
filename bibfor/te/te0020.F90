9a4d7f9c71952201db609159f574243a95abd67b
Etienne Cheignon <etienne.cheignon@necs.fr>
2014-02-26 14:52:23 +0100

    [#22055] Add test for POU_D_TGM/CHAR_MECA_ESPI and delete CHAR_MECA_EPSI for POU_C_T *** suppr var unused

--- bibfor/te/te0020.F90
+++ bibfor/te/te0020.F90
@@ -44,22 +44,19 @@ subroutine te0020(nomopt, nomte)
 !        'MECA_POU_D_T'   : SECTION VARIABLE
 !        'MECA_POU_D_TG'  : AVEC GAUCHISSEMENT
 !        'MECA_POU_D_TGM' : AVEC GAUCHISSEMENT, SECTION MULTIFIBRES
-!     POUTRE COURBE DE TIMOSHENKO
-!        'MECA_POU_C_T'   : SECTION CONSTANTE
 !     ------------------------------------------------------------------
     integer :: nbres
     parameter     (nbres=4)
-    integer :: nbfib, ind, i, j, codres(nbres)
+    integer :: nbfib, i, codres(nbres)
     integer :: lmater, inbfib, lsect, jacf, lsect2, idefi, ivectu
-    integer :: lx, iadzi, iazk24, lrcou, lorien, nnoc, nno, nc
-    real(kind=8) :: valres(nbres), zero, un, deux, r8bid, matk(78)
-    real(kind=8) :: e, xnu, g, carsec(6), fs(14), bsm(12, 12)
+    integer :: lorien, nno, nc
+    real(kind=8) :: valres(nbres), zero, un, deux, r8bid
+    real(kind=8) :: e, xnu, g, carsec(6), fs(14)
     real(kind=8) :: a, xiy, xiz, alfay, alfaz, xjx
     real(kind=8) :: a2, xiy2, xiz2
     real(kind=8) :: epx, xky, xkz
-    real(kind=8) :: rad, angarc, xfl, xfly, xflz, angs2, ang, xl
-    real(kind=8) :: along, de(12), pgl(3, 3), pgl1(3, 3), pgl2(3, 3)
-    character(len=8) :: nomres(nbres), nomail
+    real(kind=8) :: pgl(3, 3)
+    character(len=8) :: nomres(nbres)
     parameter (zero=0.0d0, un=1.0d0, deux = 2.0d0)
 !     ------------------------------------------------------------------
 !
@@ -119,137 +116,71 @@ subroutine te0020(nomopt, nomte)
     epx = zr(idefi)
     xky = zr(idefi+1)
     xkz = zr(idefi+2)
-    if (nomte .ne. 'MECA_POU_C_T') then
-        fs(1) = e*a*epx
-        fs(2) = 0.d0
-        fs(3) = 0.d0
-        fs(4) = 0.d0
-        fs(5) = e*xiy*xky
-        fs(6) = e*xiz*xkz
-        if ((nomte.eq.'MECA_POU_D_TG')) then
-            fs( 7) = 0.d0
-            fs( 8) = e*a*epx
-            fs( 9) = 0.d0
-            fs(10) = 0.d0
-            fs(11) = 0.d0
-            fs(12) = e*xiy*xky
-            fs(13) = e*xiz*xkz
-            fs(14) = 0.d0
+
+    fs(1) = e*a*epx
+    fs(2) = 0.d0
+    fs(3) = 0.d0
+    fs(4) = 0.d0
+    fs(5) = e*xiy*xky
+    fs(6) = e*xiz*xkz
+    if ((nomte.eq.'MECA_POU_D_TG')) then
+        fs( 7) = 0.d0
+        fs( 8) = e*a*epx
+        fs( 9) = 0.d0
+        fs(10) = 0.d0
+        fs(11) = 0.d0
+        fs(12) = e*xiy*xky
+        fs(13) = e*xiz*xkz
+        fs(14) = 0.d0
 !
-            nc = 7
-        else if (nomte.eq.'MECA_POU_D_TGM') then
-!           RECUPERATION DES CARACTERISTIQUES DES FIBRES
-            call pmfitx(zi(lmater), 1, carsec, r8bid)
-            fs(1) = carsec(1)*epx
-            fs(5) = carsec(5)*xky
-            fs(6) = carsec(4)*xkz
-            fs( 7) = 0.d0
-            fs( 8) = fs(1)
-            fs( 9) = 0.d0
-            fs(10) = 0.d0
-            fs(11) = 0.d0
-            fs(12) = fs(5)
-            fs(13) = fs(6)
-            fs(14) = 0.d0
+        nc = 7
+    else if (nomte.eq.'MECA_POU_D_TGM') then
+!       RECUPERATION DES CARACTERISTIQUES DES FIBRES
+        call pmfitx(zi(lmater), 1, carsec, r8bid)
+        fs(1) = carsec(1)*epx
+        fs(5) = carsec(5)*xky
+        fs(6) = carsec(4)*xkz
+        fs( 7) = 0.d0
+        fs( 8) = fs(1)
+        fs( 9) = 0.d0
+        fs(10) = 0.d0
+        fs(11) = 0.d0
+        fs(12) = fs(5)
+        fs(13) = fs(6)
+        fs(14) = 0.d0
 !
-            nc = 7
-        else if (nomte.eq.'MECA_POU_D_EM') then
-!           RECUPERATION DES CARACTERISTIQUES DES FIBRES
-            call pmfitx(zi(lmater), 1, carsec, r8bid)
-            fs(1) = carsec(1)*epx
-            fs(5) = carsec(5)*xky
-            fs(6) = carsec(4)*xkz
-            fs(7) = fs(1)
-            fs(8) = 0.d0
-            fs(9) = 0.d0
-            fs(10) = 0.d0
-            fs(11) = fs(5)
-            fs(12) = fs(6)
-        else
-            fs( 7) = e*a2*epx
-            fs( 8) = 0.d0
-            fs( 9) = 0.d0
-            fs(10) = 0.d0
-            fs(11) = e*xiy2*xky
-            fs(12) = e*xiz2*xkz
-        endif
-        fs(1) = -fs(1)
-        fs(2) = -fs(2)
-        fs(3) = -fs(3)
-        fs(4) = -fs(4)
-        fs(5) = -fs(5)
-        fs(6) = -fs(6)
+        nc = 7
+    else if (nomte.eq.'MECA_POU_D_EM') then
+!       RECUPERATION DES CARACTERISTIQUES DES FIBRES
+        call pmfitx(zi(lmater), 1, carsec, r8bid)
+        fs(1) = carsec(1)*epx
+        fs(5) = carsec(5)*xky
+        fs(6) = carsec(4)*xkz
+        fs(7) = fs(1)
+        fs(8) = 0.d0
+        fs(9) = 0.d0
+        fs(10) = 0.d0
+        fs(11) = fs(5)
+        fs(12) = fs(6)
     else
-!       COORDONNEES DES NOEUDS
-        call jevech('PGEOMER', 'L', lx)
-        lx = lx - 1
-        xl = sqrt(&
-                ( zr(lx+4)-zr(lx+1))**2 + (zr(lx+5)-zr(lx+2) )**2 + (zr(lx+6)-zr(lx+3) )**2)
-        if (xl .eq. zero) then
-            call tecael(iadzi, iazk24)
-            nomail = zk24(iazk24-1+3)(1:8)
-            call utmess('F', 'ELEMENTS2_43', sk=nomail)
-        endif
-        call jevech('PCAARPO', 'L', lrcou)
-        rad = zr(lrcou)
-        angarc = zr(lrcou+1)
-        xfl = zr(lrcou+2)
-        xfly = xfl
-        xflz = xfl
-        if (xfl .eq. zero) then
-            xfly = zr(lrcou+4)
-            xflz = zr(lrcou+6)
-        endif
-        angs2 = trigom('ASIN',xl/ (deux*rad))
-        ang = angs2*deux
-        xl = rad*ang
-        xiy = xiy/xfly
-        xiz = xiz/xflz
-        xiy2 = xiy2/xfly
-        xiz2 = xiz2/xflz
-        call ptka10(matk, e, a, xiy, xiz,&
-                    xjx, g, alfay, alfaz, rad,&
-                    ang, 1)
-!       REMPLISSAGE DE LA MATRICE CARREE
-        ind = 0
-        do i = 1, 12
-            fs(i) = zero
-            de(i) = zero
-            do j = 1, i - 1
-                ind = ind + 1
-                bsm(i,j) = matk(ind)
-                bsm(j,i) = matk(ind)
-            enddo
-            ind = ind + 1
-            bsm(i,i) = matk(ind)
-        enddo
-        along = deux*rad*epx*sin(angs2)
-        de(1) = -along*cos(angs2)
-        de(2) = along*sin(angs2)
-        de(7) = -de(1)
-        de(8) = de(2)
-!       CALCUL DES FORCES INDUITES ---
-        do i = 1, 6
-            fs(i) = 0.d0
-            fs(i+6) = 0.d0
-            do j = 1, 6
-                fs(i) = fs(i) + bsm(i,j)*de(j)
-                fs(i+6) = fs(i+6) + bsm(i+6,j+6)*de(j+6)
-            enddo
-        enddo
+        fs( 7) = e*a2*epx
+        fs( 8) = 0.d0
+        fs( 9) = 0.d0
+        fs(10) = 0.d0
+        fs(11) = e*xiy2*xky
+        fs(12) = e*xiz2*xkz
     endif
+    fs(1) = -fs(1)
+    fs(2) = -fs(2)
+    fs(3) = -fs(3)
+    fs(4) = -fs(4)
+    fs(5) = -fs(5)
+    fs(6) = -fs(6)
+
 !   RECUPERATION DES ORIENTATIONS ALPHA,BETA,GAMMA
     call jevech('PCAORIE', 'L', lorien)
 !   MATRICE DE ROTATION MGL
-    if (nomte .eq. 'MECA_POU_C_T') then
-!       POUTRE COURBE DE TIMOSKENKO A 6 DDL: COORDONNEES DES NOEUDS
-        call matro2(zr(lorien), angarc, angs2, pgl1, pgl2)
-        nnoc = 1
-        call utpvlg(nnoc, nc, pgl1, fs, zr(ivectu))
-        call utpvlg(nnoc, nc, pgl2, fs(7), zr(ivectu+6))
-    else
-        nno = 2
-        call matrot(zr(lorien), pgl)
-        call utpvlg(nno, nc, pgl, fs, zr(ivectu))
-    endif
+    nno = 2
+    call matrot(zr(lorien), pgl)
+    call utpvlg(nno, nc, pgl, fs, zr(ivectu))
 end subroutine
