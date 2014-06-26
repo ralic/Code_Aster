be7a6148babb09f5aaa8395a92a66bcb3ef865b4
Code_Aster <code-aster@edf.fr>
2014-06-26 18:23:23 +0200

    [wafcfg] add configuration of athosdev server

---
+++ wafcfg/athosdev.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+"""
+Configuration for aster5
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable.sh
+
+waf configure --use-config=aster5 --prefix=../install/std
+waf install -p
+"""
+
+import os
+ASTER_ROOT = os.environ['ASTER_ROOT']
+YAMMROOT = ASTER_ROOT + '/public/V7_4_0_201406'
+
+import intel
+
+def configure(self):
+    opts = self.options
+
+    intel.configure(self)
+
+    # enable TEST_STRICT on the reference server
+    self.env.append_value('DEFINES', ['TEST_STRICT'])
+
+    self.env.append_value('CFLAGS_ASTER_DEBUG', ['-D__DEBUG_ALL__'])
+    self.env.append_value('FCFLAGS_ASTER_DEBUG', ['-D__DEBUG_ALL__'])
+    self.env['ADDMEM'] = 250
+    self.env.append_value('OPT_ENV', [
+        '. /etc/profile.d/003_modules.sh',
+        'module load intel_compilers/14.0.0.144',
+        '. ' + ASTER_ROOT + '/etc/codeaster/profile_mfront.sh'])
+
+    self.env.append_value('LIBPATH', [
+        '/usr/lib/atlas-base/atlas',                       # for NumPy, see issue18751
+        YAMMROOT + '/prerequisites/Hdf5_1810/lib',
+        YAMMROOT + '/tools/Medfichier_307/lib',
+        YAMMROOT + '/prerequisites/Mumps_for_aster/lib',
+        YAMMROOT + '/prerequisites/Mumps_for_aster/libseq',
+        YAMMROOT + '/prerequisites/Metis_40/lib',
+        YAMMROOT + '/prerequisites/Scotch_5111/lib'])
+
+    self.env.append_value('INCLUDES', [
+        YAMMROOT + '/prerequisites/Hdf5_1810/include',
+        YAMMROOT + '/tools/Medfichier_307/include',
+        YAMMROOT + '/prerequisites/Metis_40/Lib',
+        YAMMROOT + '/prerequisites/Scotch_5111/include'])
+
+    self.env.append_value('LIB', ('pthread', 'util'))
+
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+
+    opts.enable_petsc = False
---
+++ wafcfg/athosdev_mpi.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+"""
+Configuration for athosdev + Intel MPI
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable_mpi.sh
+
+waf_mpi configure --use-config=athosdev_mpi --prefix=../install/mpi
+waf_mpi install -p
+"""
+
+import athosdev
+ASTER_ROOT = athosdev.ASTER_ROOT
+YAMMROOT = athosdev.YAMMROOT
+
+def configure(self):
+    opts = self.options
+
+    # parallel must be set before calling intel.configure() to use MPI wrappers
+    opts.parallel = True
+    athosdev.configure(self)
+    self.env['ADDMEM'] = 400
+
+    self.env.append_value('OPT_ENV', [
+        'module load intel_mpi'])
+
+    self.env.prepend_value('LIBPATH', [
+        YAMMROOT + '/prerequisites/Mumps_mpi_for_aster/lib',        
+        YAMMROOT + '/prerequisites/Petsc_mpi_petsc_aster_34/lib'])
+    self.env.prepend_value('INCLUDES', [
+        YAMMROOT + '/prerequisites/Petsc_mpi_petsc_aster_34/include'])
+
+    opts.enable_petsc = True
+
+    # allow to compile the elements catalog using the executable on one processor
+    self.env['CATALO_CMD'] = 'I_MPI_FABRICS=shm'
