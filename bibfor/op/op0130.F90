ef4e31f2d830db2601907dbb3f924e7179d25f9c
Matthias Rosier <rosier@phimeca.com>
2015-08-06 11:38:38 +0200

    [#22850] POST_DYNA_MODA_T : Add keyword GROUP_NO

--- bibfor/op/op0130.F90
+++ bibfor/op/op0130.F90
@@ -25,44 +25,54 @@ subroutine op0130()
 #include "jeveux.h"
 #include "asterc/getfac.h"
 #include "asterc/getres.h"
+#include "asterfort/dismoi.h"
 #include "asterfort/getvid.h"
 #include "asterfort/getvis.h"
 #include "asterfort/getvr8.h"
 #include "asterfort/getvtx.h"
 #include "asterfort/infmaj.h"
+#include "asterfort/jedetr.h"
 #include "asterfort/jedema.h"
 #include "asterfort/jemarq.h"
 #include "asterfort/jeveuo.h"
 #include "asterfort/pochoc.h"
 #include "asterfort/pochpv.h"
 #include "asterfort/porefd.h"
+#include "asterfort/reliem.h"
+#include "asterfort/utmess.h"
 #include "asterfort/titre.h"
-    integer :: nbbloc, nbclas, n1, n2, i, nbind
+    integer :: nbbloc, nbclas, i, nbind, nbno, jnomno
     real(kind=8) :: tdebut, tfin, offset, trepos
+    character(len=8) :: base, modele, maillage
     character(len=8) :: trange, noeu, cmp, nomres
-    character(len=16) :: nomcmd, concep, koptio
+    character(len=16) :: nomcmd, concep, koptio, motcle(2), typmcl(2)
+    character(len=24) :: nomno
     aster_logical :: loptio
     integer, pointer :: desc(:) => null()
-!     ------------------------------------------------------------------
+!   ------------------------------------------------------------------
+    data motcle  /'NOEUD','GROUP_NO'/
+    data typmcl  /'NOEUD','GROUP_NO'/
+!   ------------------------------------------------------------------
+
 !
     call jemarq()
     nomres = ' '
     call getres(nomres, concep, nomcmd)
     call infmaj()
 !
-    call getvid(' ', 'RESU_GENE', scal=trange, nbret=n1)
+    call getvid(' ', 'RESU_GENE', scal=trange)
     call jeveuo(trange//'           .DESC', 'L', vi=desc)
 !
     call getfac('CHOC', nbind)
     if (nbind .ne. 0) then
-        do 10 i = 1, nbind
-            call getvis('CHOC', 'NB_BLOC', iocc=i, scal=nbbloc, nbret=n1)
-            call getvr8('CHOC', 'INST_INIT', iocc=i, scal=tdebut, nbret=n1)
-            call getvr8('CHOC', 'INST_FIN', iocc=i, scal=tfin, nbret=n1)
-            call getvr8('CHOC', 'SEUIL_FORCE', iocc=i, scal=offset, nbret=n1)
-            call getvr8('CHOC', 'DUREE_REPOS', iocc=i, scal=trepos, nbret=n1)
-            call getvtx('CHOC', 'OPTION', iocc=i, scal=koptio, nbret=n1)
-            call getvis('CHOC', 'NB_CLASSE', iocc=i, scal=nbclas, nbret=n1)
+        do i = 1, nbind
+            call getvis('CHOC', 'NB_BLOC', iocc=i, scal=nbbloc)
+            call getvr8('CHOC', 'INST_INIT', iocc=i, scal=tdebut)
+            call getvr8('CHOC', 'INST_FIN', iocc=i, scal=tfin)
+            call getvr8('CHOC', 'SEUIL_FORCE', iocc=i, scal=offset)
+            call getvr8('CHOC', 'DUREE_REPOS', iocc=i, scal=trepos)
+            call getvtx('CHOC', 'OPTION', iocc=i, scal=koptio)
+            call getvis('CHOC', 'NB_CLASSE', iocc=i, scal=nbclas)
             if (koptio(1:6) .eq. 'USURE') then
                 loptio = .true.
             else
@@ -75,18 +85,30 @@ subroutine op0130()
                 call pochpv(trange, nbbloc, tdebut, tfin, offset,&
                             trepos, nbclas, nomres, loptio)
             endif
- 10     continue
+        end do
     endif
 !
     call getfac('RELA_EFFO_DEPL', nbind)
+    nomno = '&&OP0130.MES_NOEUDS'
     if (nbind .ne. 0 .and. desc(4) .ne. 0) then
-        do 20 i = 1, nbind
-            call getvtx('RELA_EFFO_DEPL', 'NOEUD', iocc=i, scal=noeu, nbret=n2)
-            call getvtx('RELA_EFFO_DEPL', 'NOM_CMP', iocc=i, scal=cmp, nbret=n2)
+        do i = 1, nbind
+                call dismoi('BASE_MODALE', trange, 'RESU_DYNA', repk=base, arret='F')
+                call dismoi('NOM_MODELE', base, 'RESULTAT', repk=modele)
+                call dismoi('NOM_MAILLA', base, 'RESULTAT', repk=maillage)
+!
+                call reliem(modele, maillage, 'NO_NOEUD', 'RELA_EFFO_DEPL', i,&
+                            2, motcle, typmcl, nomno, nbno)
+                call jeveuo(nomno, 'L', jnomno)
+                if (nbno .ne. 1) then
+                    call utmess('F', 'MODELISA5_67', sk='RELA_EFFO_DEPL', si=nbno)
+                endif
+                noeu = zk8(jnomno)
+            call getvtx('RELA_EFFO_DEPL', 'NOM_CMP', iocc=i, scal=cmp)
 !
             call porefd(trange, noeu, cmp, nomres)
- 20     continue
+        end do
     endif
+    call jedetr(nomno)
 !
     call titre()
 !
