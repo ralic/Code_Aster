fe466d46c59bd219ad20780991ffec0c9485c6be
Mickaël ABBAS <mickael.abbas@edf.fr>
2015-09-30 07:50:24 +0200

    [#22392] Select analytic integration rule for GRANGER behavior

--- bibpyt/Comportement/kit_ddi.py
+++ bibpyt/Comportement/kit_ddi.py
@@ -29,7 +29,7 @@ loi = LoiComportement(
     modelisation=('3D', 'AXIS', 'D_PLAN'),
     deformation = ('PETIT', 'PETIT_REAC', 'GROT_GDEP'),
     nom_varc = ('TEMP', 'SECH', 'HYDR'),
-    algo_inte = ('SANS_OBJET',),
+    algo_inte = ('SPECIFIQUE',),
     type_matr_tang = ('PERTURBATION', 'VERIFICATION'),
     proprietes = None,
 )
