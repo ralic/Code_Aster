25d53f984a11a103a16d62fcd3beb689ee70fe6c
Hassan Berro <hassan.berro@edf.fr>
2015-12-15 12:37:59 +0100

    [#24606] Cleanup - remove vpmain for total mass calculation for eigen vector dir. params

--- bibfor/op/op0037.F90
+++ bibfor/op/op0037.F90
@@ -2,7 +2,7 @@ subroutine op0037()
     implicit none
 !     ------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -57,7 +57,6 @@ subroutine op0037()
 #include "asterfort/utnono.h"
 #include "asterfort/vpcrea.h"
 #include "asterfort/vpddl.h"
-#include "asterfort/vpmain.h"
 #include "asterfort/vpnor1.h"
 #include "asterfort/vpnor2.h"
 #include "asterfort/vpnorm.h"
