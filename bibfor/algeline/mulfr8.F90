ca4d6312b86a16cc8272288d01540dbdffb9b64f
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-01-12 09:11:22 +0100

    [#24556] update copyright date

--- bibfor/algeline/mulfr8.F90
+++ bibfor/algeline/mulfr8.F90
@@ -2,7 +2,7 @@ subroutine mulfr8(nommat, npivot, neq, typsym, eps,&
                   renumz)
 ! person_in_charge: olivier.boiteau at edf.fr
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
--- bibfor/utilifor/utmess_core.F90
+++ bibfor/utilifor/utmess_core.F90
@@ -1,7 +1,7 @@
 subroutine utmess_core(typ, idmess, nk, valk, ni,&
                        vali, nr, valr, fname)
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
