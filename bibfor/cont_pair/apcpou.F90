652553fc694083de2c10e04f1627f071243201f0
Mickaël ABBAS <mickael.abbas@edf.fr>
2016-01-19 13:39:06 +0100

    [#21602] Refactoring node-segment pairing

--- bibfor/cont_pair/apcpou.F90
+++ bibfor/cont_pair/apcpou.F90
@@ -12,7 +12,7 @@ implicit none
 #include "blas/dcopy.h"
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -60,6 +60,8 @@ implicit none
 !
 ! --------------------------------------------------------------------------------------------------
 !
+    tau1(1:3) = 0.d0
+    tau2(1:3) = 0.d0
 !
 ! - Type of normal
 !
