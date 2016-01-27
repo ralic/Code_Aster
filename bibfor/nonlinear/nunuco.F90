a9dd2f798171416d5695e1c258fab426721b7d65
Mickaël ABBAS <mickael.abbas@edf.fr>
2016-01-27 07:19:31 +0100

    [#21602] Move some subroutines to nonlinear directory

--- bibfor/nonlinear/nunuco.F90
+++ bibfor/nonlinear/nunuco.F90
@@ -10,7 +10,7 @@ implicit none
 #include "asterfort/as_allocate.h"
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -32,7 +32,7 @@ implicit none
 !
 ! --------------------------------------------------------------------------------------------------
 !
-! ROUTINE MECA_NON_LINE (INITIALISATION)
+! Non-linear algorithm - Initializations
 !
 ! Get position of contact dof
 !
--- bibfor/nonlinear/nurota.F90
+++ bibfor/nonlinear/nurota.F90
@@ -22,7 +22,7 @@ implicit none
 #include "asterfort/as_allocate.h"
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -46,7 +46,7 @@ implicit none
 !
 ! --------------------------------------------------------------------------------------------------
 !
-! ROUTINE MECA_NON_LINE (INITIALISATION)
+! Non-linear algorithm - Initializations
 !
 ! Get position of large rotation dof 
 !
