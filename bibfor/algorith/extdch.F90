2eb3c7b543e419a9d28cbc7ee07ce47200bdfef4
Eric Lorentz <eric.lorentz@edf.fr>
2016-02-16 18:13:24 +0100

    [#24829] Correction of extdch (max of a field increment)
    Among the strategies used by the macro CALC_ENDO, one relies on the criterion
    DELTA_GRANDEUR in the time-step adaptation. But the results provided by the
    subroutine EXTDCH which computes the maximum of the increment of the internal
    variables is erroneous. The bug is apparently related to the fact that the
    storage orders of the field at the beginning and the end of the time-step are
    different which does not seem consistent with the use of BARYCH.
    
    Therefore, a more basic (and robust) way to compute the difference between two
    EL* fields is introduced is EXTDCH which solves the problem.

--- bibfor/algorith/extdch.F90
+++ bibfor/algorith/extdch.F90
@@ -1,7 +1,7 @@
 subroutine extdch(typext, valinc, nocham, nocmp, dval)
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -62,17 +62,17 @@ subroutine extdch(typext, valinc, nocham, nocmp, dval)
 !
     integer ::  jcnsl
     integer :: nbno, ino
-    integer :: jcesd, jcesl
-    integer :: nbma, ima, ipt, isp, icmp, nbpt, nbsp, nbcmp, iad
-    real(kind=8) :: valeur
+    integer :: nbma, ima, ipt, isp, icmp, nbpt, nbsp, nbcmp
+    integer :: jmoid,jmoil,jmoiv,imoiad
+    integer :: jplud,jplul,jpluv,ipluad
+    real(kind=8) :: valeur,vmoi,vplu
     character(len=6) :: nompro
     character(len=16) :: typch
-    character(len=19) :: dch, dchs, chplu, chmoi
+    character(len=19) :: dch, dchs, chplu, chmoi,chmois,chplus
     parameter   (nompro = 'EXTDCH')
     aster_logical :: bool
     real(kind=8), pointer :: cnsv(:) => null()
     integer, pointer :: cnsd(:) => null()
-    real(kind=8), pointer :: cesv(:) => null()
 !
 !      REAL*8  TMP
 !
@@ -109,37 +109,53 @@ subroutine extdch(typext, valinc, nocham, nocmp, dval)
 !
     if (typext .eq. 'MAX') dval = r8miem()
     if (typext .eq. 'MAX_ABS') dval = 0.d0
-!
-!     CALCUL DE L'INCREMENT DU CHAMP
-!     DCH = CHPLU - CHMOI
-    dch = '&&'//nompro//'.DELTACH   '
-    dchs = '&&'//nompro//'.DELTACHS  '
-    call barych(chplu, chmoi, 1.d0, -1.d0, dch,&
-                'V')
+       
+    
 !
 !     ON APPELLERA MEMAX QUAND CETTE ROUTINE SERA MIEUX PROGRAMMEE
     if (typch(1:7) .eq. 'CHAM_EL') then
 !
-        call celces(dch, 'V', dchs)
-        call cesred(dchs, 0, [0], 1, nocmp,&
-                    'V', dchs)
-        call jeveuo(dchs//'.CESD', 'L', jcesd)
-        call jeveuo(dchs//'.CESL', 'L', jcesl)
-        call jeveuo(dchs//'.CESV', 'L', vr=cesv)
-        nbma = zi(jcesd-1+1)
+        chmois = '&&'//nompro//'.CHMOIS'
+        chplus = '&&'//nompro//'.CHPLUS'
+
+        call celces(chmoi,'V',chmois)
+        call cesred(chmois, 0, [0], 1, nocmp,'V',chmois)
+        call celces(chplu,'V',chplus)  
+        call cesred(chplus, 0, [0], 1, nocmp,'V',chplus)
+        
+        call jeveuo(chmois//'.CESD', 'L', jmoid)
+        call jeveuo(chplus//'.CESD', 'L', jplud)
+        call jeveuo(chmois//'.CESL', 'L', jmoil)
+        call jeveuo(chplus//'.CESL', 'L', jplul)
+        call jeveuo(chmois//'.CESV', 'L', jmoiv)
+        call jeveuo(chplus//'.CESV', 'L', jpluv)
+
+        nbma = zi(jmoid-1+1)  
+        ASSERT(zi(jplud-1+1).eq.nbma)        
+        
         do 40,ima = 1,nbma
-        nbpt = zi(jcesd-1+5+4*(ima-1)+1)
-        nbsp = zi(jcesd-1+5+4*(ima-1)+2)
-        nbcmp = zi(jcesd-1+5+4*(ima-1)+3)
+        nbpt  = zi(jmoid-1+5+4*(ima-1)+1)
+        nbsp  = zi(jmoid-1+5+4*(ima-1)+2)
+        nbcmp = zi(jmoid-1+5+4*(ima-1)+3)
+        
+        ASSERT(zi(jplud-1+5+4*(ima-1)+1) .eq. nbpt)
+        ASSERT(zi(jplud-1+5+4*(ima-1)+2) .eq. nbsp)
+        ASSERT(zi(jplud-1+5+4*(ima-1)+3) .eq. nbcmp)
+        
         do 30,ipt = 1,nbpt
         do 20,isp = 1,nbsp
         do 10,icmp = 1,nbcmp
-        call cesexi('C', jcesd, jcesl, ima, ipt,&
-                    isp, icmp, iad)
-        if (iad .gt. 0) then
-            valeur = cesv(iad)
-!
-!
+        call cesexi('C',jmoid,jmoil,ima,ipt,isp,1,imoiad)
+        call cesexi('C',jplud,jplul,ima,ipt,isp,1,ipluad)
+                    
+        if (imoiad.gt.0 .or. ipluad.gt.0) then
+
+            ASSERT(imoiad.gt.0 .and. ipluad.gt.0)
+           
+            vmoi = zr(jmoiv-1+imoiad)
+            vplu = zr(jpluv-1+ipluad)    
+            valeur = vplu-vmoi   
+            
             if (typext(5:7) .eq. 'ABS') valeur = abs( valeur)
             if (typext(5:7) .eq. 'VAR') then
                 if (abs(valeur) .gt. r8prem()) then
@@ -166,6 +182,12 @@ subroutine extdch(typext, valinc, nocham, nocmp, dval)
 !
     else if (typch.eq.'CHAM_NO') then
 !
+!       CALCUL DE L'INCREMENT DU CHAMP
+!       DCH = CHPLU - CHMOI
+        dch    = '&&'//nompro//'.DELTACH   '
+        dchs   = '&&'//nompro//'.DELTACHS  '        
+        call barych(chplu, chmoi, 1.d0, -1.d0, dch,'V')
+
         call cnocns(dch, 'V', dchs)
         call cnsred(dchs, 0, [0], 1, nocmp,&
                     'V', dchs)
