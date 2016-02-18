591f96bcae0e6339f8c9aa4e04ef3aa30a59d32c
Véronique Le Corvec <vlc@necs.fr>
2016-02-18 16:50:47 +0100

    [#25773] Unitialised value rcvale.F90

--- bibfor/modelisa/rcvale.F90
+++ bibfor/modelisa/rcvale.F90
@@ -1,7 +1,7 @@
 subroutine rcvale(nommaz, phenom, nbpar, nompar, valpar,&
                   nbres, nomres, valres, icodre, iarret)
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -93,6 +93,7 @@ subroutine rcvale(nommaz, phenom, nbpar, nompar, valpar,&
     nommat = nommaz
     phen = phenom
     kstop='F '
+    k11 = ''
 !
     ASSERT(iarret.ge.0 .and. iarret.le.2)
 !
