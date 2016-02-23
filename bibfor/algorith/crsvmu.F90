f7d617f37f36eb68ddda76ec1c984440847272df
Jacques Pellet <jacques.pellet@edf.fr>
2016-02-23 13:26:14 +0100

    [#22204] Update the Copyrights.

--- bibfor/algorith/crsvmu.F90
+++ bibfor/algorith/crsvmu.F90
@@ -27,7 +27,7 @@ subroutine crsvmu(motfac, solveu, istop, nprec,&
     character(len=16) :: motfac
     character(len=19) :: solveu
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
