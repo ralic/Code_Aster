91ccdddf589fea6842d16512d9002974ed3871cf
Code_Aster <code-aster@edf.fr>
2016-03-09 09:54:03 +0100

    [repo] wafcfg files for Calibre9 mpi

---
+++ wafcfg/calibre9_mpi.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+"""
+Configuration for Calibre 9  MPI
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable_mpi.sh
+
+waf_mpi configure --use-config=calibre9_mpi --prefix=../install/mpi
+waf_mpi install -p
+"""
+
+import calibre9
+YAMMROOT = calibre9.YAMMROOT
+
+def configure(self):
+    opts = self.options
+
+    opts.parallel = True
+    calibre9.configure(self)
+    self.env['ADDMEM'] = 400
+
+    self.env.prepend_value('LIBPATH', [
+        YAMMROOT + '/prerequisites/Mumps-501_consortium_aster3/MPI/lib',
+        YAMMROOT + '/prerequisites/Petsc_mpi-petsc_aster/lib',
+    ])
+
+    self.env.prepend_value('INCLUDES', [
+        YAMMROOT + '/prerequisites/Mumps-501_consortium_aster3/MPI/include',
+        YAMMROOT + '/prerequisites/Petsc_mpi-petsc_aster/include',
+    ])
+
+    opts.enable_petsc = True
