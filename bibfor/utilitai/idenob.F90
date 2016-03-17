b9d0ce1d2e5793d9b7445bec9d5d2c32d1444999
Jacques Pellet <jacques.pellet@edf.fr>
2016-03-17 17:10:34 +0100

    [#24948] Update Copyrights for aslint

--- bibfor/utilitai/idenob.F90
+++ bibfor/utilitai/idenob.F90
@@ -16,7 +16,7 @@ function idenob(obj1, obj2)
     character(len=*) :: obj1, obj2
 
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
