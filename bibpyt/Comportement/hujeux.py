8a27b4f0238fce9f209eb8e549b63687b103dadc
Vinicius Alves-Fernandes <vinicius.alves-fernandes@edf.fr>
2016-04-12 15:07:05 +0200

    [#23654] GDEF_LOG open to Hujeux soil behaviour

--- bibpyt/Comportement/hujeux.py
+++ bibpyt/Comportement/hujeux.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+# COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -40,7 +40,7 @@ loi = LoiComportement(
         'XHXY1', 'XHXY2', 'THXY1', 'THXY2', 'RHYZ'),
     mc_mater = ('ELAS', 'HUJEUX'),
     modelisation = ('3D', 'THM', 'D_PLAN'),
-    deformation = ('PETIT', 'PETIT_REAC', 'GROT_GDEP'),
+    deformation = ('PETIT', 'PETIT_REAC', 'GROT_GDEP', 'GDEF_LOG'),
     nom_varc = ('TEMP',),
     algo_inte = ('NEWTON', 'NEWTON_PERT', 'NEWTON_RELI', 'SPECIFIQUE'),
     type_matr_tang = ('PERTURBATION', 'VERIFICATION'),
