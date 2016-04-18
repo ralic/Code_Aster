64bec308a0f225a4cac9748c4eedbfe4237c8367
Etienne Cheignon <etienne.cheignon@necs.fr>
2016-04-18 16:10:59 +0200

    [#25056] Remove acevca.F90 and acevca.h

--- bibpyt/Messages/modelisa.py
+++ bibpyt/Messages/modelisa.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+# COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -249,12 +249,6 @@ La caractéristique %(k1)s, coefficient de cisaillement, pour les poutres doit t
  occurrence  %(k1)s
  section " %(k2)s
  valeur  %(k3)s  de "VALE" non admise (valeur test interne)
-"""),
-
-    52 : _(u"""
- CABLE :
- occurrence 1
- le mot clé "section" est obligatoire.
 """),
 
     53 : _(u"""
