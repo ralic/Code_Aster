073374c767900495143f3e38d736fbc6046832de
Thomas DE-SOZA <thomas.de-soza at edf.fr>
2016-05-13 09:32:29 +0200

    [#24583] use a buffer when changing shear forces reference frame in plates - this prevents an error when changing forces in-place - the companion subroutine for shear stresses is refactored so that both match

--- bibfor/elements/dxefro.F90
+++ bibfor/elements/dxefro.F90
@@ -1,6 +1,6 @@
 subroutine dxefro(ne, t2iu, edgle, edglc)
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -17,6 +17,7 @@ subroutine dxefro(ne, t2iu, edgle, edglc)
 ! ======================================================================
     implicit none
 #include "asterfort/utbtab.h"
+#include "asterfort/pmavec.h"
     integer, intent(in) :: ne
     real(kind=8), intent(in) :: t2iu(2, 2)
     real(kind=8), intent(in) :: edgle(*)
@@ -27,19 +28,28 @@ subroutine dxefro(ne, t2iu, edgle, edglc)
 !     ------------------------------------------------------------------
 !     IN  NE    I      NOMBRE DE POINTS A TRAITER
 !     IN  T2IU  R 2,2  MATRICE DE PASSAGE INTRINSEQUE - UTILISATEUR
-!     IN  EDGLE R  8   NXX NYY NXY MXX MYY MXY VX VY
-!     OUT EDGLC R  8   NXX NYY NXY MXX MYY MXY VX VY
+!     IN  EDGLE R  8   NXX NYY NXY MXX MYY MXY QX QY
+!     OUT EDGLC R  8   NXX NYY NXY MXX MYY MXY QX QY
 !  OU IN  EDGLE R  8   EXX EYY EXY KXX KYY KXY GAX GAY
 !     OUT EDGLE R  8   EXX EYY EXY KXX KYY KXY GAX GAY
 !     
 !     ATTENTION : LES DEFORMATIONS GENERALISEES SONT ATTENDUES SOUS LA FORME
-!     EXX EYY EXY ET NON PAS SOUS LA FORME EXX EYY 2EXY.  
+!     EXX EYY EXY ET NON PAS SOUS LA FORME EXX EYY 2EXY.
+!
+!  REMARQUE : ON PEUT APPELER CETTE ROUTINE AVEC LE MEME TABLEAU EN E/S
 !     ------------------------------------------------------------------
-    real(kind=8) :: nle(2,2), mle(2,2), xab(2, 2)
-    real(kind=8) :: nlc(2,2), mlc(2,2)
+    real(kind=8) :: nle(2,2), mle(2,2), qle(2)
+    real(kind=8) :: nlc(2,2), mlc(2,2), qlc(2)
+    real(kind=8) ::t2ui(2,2), xab(2,2)
     integer :: i
 !
-    do 120 i = 1, ne
+!   Transposée de t2iu pour changement de repère des efforts tranchants
+    t2ui(1,1) = t2iu(1,1)
+    t2ui(2,1) = t2iu(1,2)
+    t2ui(1,2) = t2iu(2,1)
+    t2ui(2,2) = t2iu(2,2)
+!
+    do i = 1, ne
         nle(1,1) = edgle(1+8*(i-1))
         nle(2,1) = edgle(3+8*(i-1))
         nle(1,2) = edgle(3+8*(i-1))
@@ -50,10 +60,12 @@ subroutine dxefro(ne, t2iu, edgle, edglc)
         mle(1,2) = edgle(6+8*(i-1))
         mle(2,2) = edgle(5+8*(i-1))
 !
-        call utbtab('ZERO', 2, 2, nle, t2iu,&
-                    xab, nlc)
-        call utbtab('ZERO', 2, 2, mle, t2iu,&
-                    xab, mlc)
+        qle(1)   = edgle(7+8*(i-1))
+        qle(2)   = edgle(8+8*(i-1))
+!
+        call utbtab('ZERO', 2, 2, nle, t2iu, xab, nlc)
+        call utbtab('ZERO', 2, 2, mle, t2iu, xab, mlc)
+        call pmavec('ZERO', 2, t2ui, qle, qlc)
 !
         edglc(1+8*(i-1)) = nlc(1,1)
         edglc(2+8*(i-1)) = nlc(2,2)
@@ -63,7 +75,7 @@ subroutine dxefro(ne, t2iu, edgle, edglc)
         edglc(5+8*(i-1)) = mlc(2,2)
         edglc(6+8*(i-1)) = mlc(2,1)
 !
-        edglc(7+8*(i-1)) = edgle( 7+8*(i-1)) * t2iu(1, 1) + edgle(8+8*( i-1)) * t2iu(2, 1 )
-        edglc(8+8*(i-1)) = edgle( 7+8*(i-1)) * t2iu(1, 2) + edgle(8+8*( i-1)) * t2iu(2, 2 )
-120  end do
+        edglc(7+8*(i-1)) = qlc(1)
+        edglc(8+8*(i-1)) = qlc(2)
+    end do
 end subroutine
--- bibfor/elements/dxsiro.F90
+++ bibfor/elements/dxsiro.F90
@@ -1,6 +1,6 @@
 subroutine dxsiro(ne, t2iu, tensav, tensap)
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -17,6 +17,7 @@ subroutine dxsiro(ne, t2iu, tensav, tensap)
 ! ======================================================================
     implicit none
 #include "asterfort/utbtab.h"
+#include "asterfort/pmavec.h"
     integer, intent(in) :: ne
     real(kind=8), intent(in) :: t2iu(2, 2)
     real(kind=8), intent(in) :: tensav(*)
@@ -32,13 +33,19 @@ subroutine dxsiro(ne, t2iu, tensav, tensap)
 !     IN  TENSAV R    *   XX  YY  ZZ  XY  XZ  YZ
 !     OUT TENSAP R    *   XX  YY  ZZ  XY  XZ  YZ
 !
-!  REMARQUE : ON PEUT APPELER CETTE ROUTINE AVEC UN TABLEAU EN IN/OUT
+!  REMARQUE : ON PEUT APPELER CETTE ROUTINE AVEC LE MEME TABLEAU EN E/S
 !     ------------------------------------------------------------------
-    real(kind=8) :: sigmav(2, 2), sigmap(2, 2), tampon(2)
-    real(kind=8) :: xab(2, 2)
+    real(kind=8) :: sigmav(2, 2), sigmap(2, 2), cisaav(2), cisaap(2)
+    real(kind=8) :: t2ui(2 ,2), xab(2, 2)
 !-----------------------------------------------------------------------
     integer :: i
 !-----------------------------------------------------------------------
+!
+!   Transposée de t2iu pour changement de repère des efforts tranchants
+    t2ui(1,1) = t2iu(1,1)
+    t2ui(2,1) = t2iu(1,2)
+    t2ui(1,2) = t2iu(2,1)
+    t2ui(2,2) = t2iu(2,2)
 !
     do i = 1, ne
         sigmav(1,1) = tensav(1+6*(i-1))
@@ -46,18 +53,19 @@ subroutine dxsiro(ne, t2iu, tensav, tensap)
         sigmav(1,2) = tensav(4+6*(i-1))
         sigmav(2,2) = tensav(2+6*(i-1))
 !
-        call utbtab('ZERO', 2, 2, sigmav, t2iu,&
-                    xab, sigmap)
+        cisaav(1)=tensav(5+6*(i-1))
+        cisaav(2)=tensav(6+6*(i-1))
+!
+        call utbtab('ZERO', 2, 2, sigmav, t2iu, xab, sigmap)
+        call pmavec('ZERO', 2, t2ui, cisaav, cisaap)
 !
         tensap(1+6*(i-1)) = sigmap(1,1)
         tensap(2+6*(i-1)) = sigmap(2,2)
         tensap(3+6*(i-1)) = tensav(3+6*(i-1))
         tensap(4+6*(i-1)) = sigmap(2,1)
 !
-        tampon(1)=tensav(5+6*(i-1))
-        tampon(2)=tensav(6+6*(i-1))
-        tensap(5+6*(i-1)) = tampon(1) * t2iu(1,1) + tampon(2) * t2iu( 2,1)
-        tensap(6+6*(i-1)) = tampon(1) * t2iu(1,2) + tampon(2) * t2iu( 2,2)
+        tensap(5+6*(i-1)) = cisaap(1)
+        tensap(6+6*(i-1)) = cisaap(2)
 !
     enddo
 !
