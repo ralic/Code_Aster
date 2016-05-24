6427047e2c8c07772d3328b47ec6b88550d577a2
Guilhem Ferte <guilhem.ferte at edf.fr>
2016-05-24 15:06:28 +0200

    [#25041] removing unused routine dclhoa.F90

--- bibfor/elements/d2cro2.F90
+++ bibfor/elements/d2cro2.F90
@@ -5,7 +5,7 @@ subroutine d2cro2(zimat, nmnbn, nmplas, nmdpla, nmddpl,&
     implicit none
 ! ======================================================================
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -207,7 +207,7 @@ subroutine d2cro2(zimat, nmnbn, nmplas, nmdpla, nmddpl,&
         goto 100
     endif
 !
-    if (nbroot .gt. 1) then
+    if (nbroot .ge. 1) then
         do 85, i=1,nbroot
         normxy(i)=abs(x(i))+abs(y(i))
 85      continue
--- bibfor/elements/dclass.F90
+++ bibfor/elements/dclass.F90
@@ -3,7 +3,7 @@ subroutine dclass(n, tab, iran)
     implicit none
 !-----------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -20,28 +20,23 @@ subroutine dclass(n, tab, iran)
 !======================================================================
 !
 !     EVALUE L ORDRE DES RACINES DANS IRAN
+!     TRI PAR UNE METHODE SOMMAIRE
+!     A NE PAS UTILISER AVEC DE GROS TABLEAUX
 !
 ! IN  N : NOMBRE DE RACINE DU POLYNOME
 ! IN  TAB : TABLEAU DES RACINES DU POLYNOME
 !
 ! OUT IRAN : RANG DE LA RACINE
 !
-#include "asterfort/dclhoa.h"
 #include "asterfort/dclsma.h"
-    integer :: n, iran(*), lim
+    integer :: n, iran(*)
 !
     real(kind=8) :: tab(*)
-!
-    lim = 8
 !
     if (n .le. 1) then
         iran(1) = 1
     else
-        if (n .ge. lim) then
-            call dclhoa(n, tab, iran)
-        else
-            call dclsma(n, tab, iran)
-        endif
+        call dclsma(n, tab, iran)
     endif
 !
 end subroutine
