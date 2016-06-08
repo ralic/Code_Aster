51950de829ad3292a46851fdb695d275deda73fd
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-06-08 16:32:43 +0200

    [#13913] add default FORMAT='ASTER' in ENGENDRE_TEST

--- bibfor/op/op0178.F90
+++ bibfor/op/op0178.F90
@@ -2,7 +2,7 @@ subroutine op0178()
     implicit none
 ! ----------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -66,7 +66,6 @@ subroutine op0178()
         call ulopen(ific, ' ', nomfi, 'NEW', 'O')
     endif
 !
-    form0 = 'ASTER'
     call getvtx(' ', 'FORMAT', scal=form0)
 !
     call getvtx(' ', 'FORMAT_R', scal=formr)
