32cd7544e946a1c11356a2a1eb09adab5e995d58
Mickael Abbas <mickael.abbas@edf.fr>
2016-10-05 09:51:46 +0200

    [#25625] Correct OBSERVATION/SUIVI_DDL when mixe nodal and elementary fields

--- bibfor/nonlinear/nmextr_ligr.F90
+++ bibfor/nonlinear/nmextr_ligr.F90
@@ -11,7 +11,7 @@ implicit none
 #include "asterfort/jeveuo.h"
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -54,10 +54,11 @@ implicit none
     integer :: nb_elem_mesh, nb_elem_ligr, nb_elem, nume_elem
     character(len=2) :: chaine
     character(len=24) :: ligrel, list_elem
-    character(len=24) :: field_disc, field_type
+    character(len=24) :: field_disc, field_type, field_comp, field
     character(len=14) :: sdextr
-    character(len=24) :: extr_comp, extr_info
+    character(len=24) :: extr_comp, extr_info, extr_field
     character(len=24), pointer :: v_extr_comp(:) => null()
+    character(len=24), pointer :: v_extr_field(:) => null()
     integer, pointer :: v_extr_info(:) => null()
     integer, pointer :: list_elem_mesh(:) => null()
     integer, pointer :: list_elem_ligr(:) => null()
@@ -74,6 +75,8 @@ implicit none
     call jeveuo(extr_info , 'L' , vi   = v_extr_info) 
     extr_comp = sdextr(1:14)//'     .COMP'
     call jeveuo(extr_comp, 'L', vk24 = v_extr_comp)
+    extr_field = sdextr(1:14)//'     .CHAM'
+    call jeveuo(extr_field, 'L' , vk24 = v_extr_field)
 !
 ! - Create list of elements
 !
@@ -85,6 +88,7 @@ implicit none
 !
 ! ----- Get info
 !
+        field_comp = v_extr_comp(4*(i_field_comp-1)+1)
         field_disc = v_extr_comp(4*(i_field_comp-1)+2)
         field_type = v_extr_comp(4*(i_field_comp-1)+3)
         ligrel     = v_extr_comp(4*(i_field_comp-1)+4)
@@ -95,20 +99,20 @@ implicit none
 !
         nb_elem_ligr = 0
         do i_keyw_fact = 1, nb_keyw_fact
-!
-! --------- Complete list of elements on all keywords
-!
-            call impfoi(0, 2, i_keyw_fact, chaine)
-            list_elem = sdextr(1:14)//chaine(1:2)//'   .MAIL'
-            call jeveuo(list_elem, 'L', vi = v_list_elem)
-            nb_elem   = v_extr_info(7+7*(i_keyw_fact-1)+3)
-            do i_elem = 1, nb_elem
-                nume_elem = v_list_elem(i_elem)
-                if (list_elem_mesh(nume_elem).eq.0) then
-                    nb_elem_ligr = nb_elem_ligr+1
-                    list_elem_mesh(nume_elem) = 1
-                endif
-            end do
+            field = v_extr_field(4*(i_keyw_fact-1)+4)
+            if (field .eq. field_comp) then
+                call impfoi(0, 2, i_keyw_fact, chaine)
+                list_elem = sdextr(1:14)//chaine(1:2)//'   .MAIL'
+                call jeveuo(list_elem, 'L', vi = v_list_elem)
+                nb_elem   = v_extr_info(7+7*(i_keyw_fact-1)+3)
+                do i_elem = 1, nb_elem
+                    nume_elem = v_list_elem(i_elem)
+                    if (list_elem_mesh(nume_elem).eq.0) then
+                        nb_elem_ligr = nb_elem_ligr+1
+                        list_elem_mesh(nume_elem) = 1
+                    endif
+                end do
+            endif
         end do
 !
 ! ----- Create list of elements
