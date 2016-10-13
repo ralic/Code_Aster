2de5702b541d7daa74dad53064cbb2cd6ba00d18
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-10-13 13:11:46 +0200

    [#25666] Fix checking of COMPOR field (since #25087).

--- bibpyt/SD/sd_cham_mater.py
+++ bibpyt/SD/sd_cham_mater.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+# COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -175,4 +175,4 @@ class sd_cham_mater(AsBase):
             sd2.check(checker)
 
             for x1 in v1[7:]:
-                assert x1 == '', v1
+                assert x1 in ('', 'VIDE', '0'), v1
