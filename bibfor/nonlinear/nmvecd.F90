2b2087d2342cb038a07329b58ea27ae9984a7739
Véronique Le Corvec <vlc@necs.fr>
2016-11-18 12:31:43 +0100

    [#25773] Unitialised value in nmvecd.F90

--- bibfor/nonlinear/nmvecd.F90
+++ bibfor/nonlinear/nmvecd.F90
@@ -6,7 +6,7 @@ subroutine nmvecd(imate, mate, nmat, matcst, loi,&
                   drpdp, etatf, ier)
 !-----------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -122,6 +122,7 @@ subroutine nmvecd(imate, mate, nmat, matcst, loi,&
     call r8inir(np*nb, 0.d0, drpdb, 1)
     call r8inir(nb*nb, 0.d0, drbdb, 1)
     call r8inir(nb*np, 0.d0, drbdp, 1)
+    call r8inir(6    , 0.d0, dxhidb,1)
     call r8inir(nb, 0.d0, rb, 1)
     call r8inir(np, 0.d0, rp, 1)
     call lceqvn(nb, ep(1), epsth)
