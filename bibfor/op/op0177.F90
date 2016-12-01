bf013a883d83a20b449054b9827b5e90d32980dd
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-12-01 11:33:52 +0100

    [#25820] TEST_TABLE did read VALE_CALC_K instead of VALE_REFE_K.

--- bibfor/op/op0177.F90
+++ bibfor/op/op0177.F90
@@ -1,7 +1,7 @@
 subroutine op0177()
 ! ----------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -194,7 +194,7 @@ subroutine op0177()
             nref = -nkr
             call jedetr(travkr)
             call wkvect(travkr, 'V V K80', nref, irefkr)
-            call getvtx(' ', 'VALE_CALC_K', nbval=nref, vect=zk80(irefkr))
+            call getvtx(' ', 'VALE_REFE_K', nbval=nref, vect=zk80(irefkr))
         endif
     endif
     if (skip .and. .not. lref) then
