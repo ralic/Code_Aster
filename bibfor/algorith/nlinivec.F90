fb02e2102036dbc9afc7a5f22665cc2ea95faea4
Fabien Grange <fabien.grange@edf.fr>
2017-02-03 13:07:43 +0100

    [#25921] Modification of dtminivec, the address memory can be negative

--- bibfor/algorith/nlinivec.F90
+++ bibfor/algorith/nlinivec.F90
@@ -3,7 +3,7 @@ subroutine nlinivec(sd_nl_, ip, lonvec, iocc, vi,&
     implicit none
 !-----------------------------------------------------------------------
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2017  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -180,7 +180,6 @@ subroutine nlinivec(sd_nl_, ip, lonvec, iocc, vi,&
         end if
     end if
     if (present(address)) then
-        ASSERT(add.gt.0)
         address = add
     end if
 
