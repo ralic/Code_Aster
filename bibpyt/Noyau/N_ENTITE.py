29c82c26c2778b8e8fcb5e6c3b5730a863cc8503
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-03-02 09:04:34 +0100

    [#26133] Enforce 'statut' to be hidden for the 'reuse' keyword.

--- bibpyt/Noyau/N_ENTITE.py
+++ bibpyt/Noyau/N_ENTITE.py
@@ -213,8 +213,8 @@ class ENTITE:
         if self.statut not in into:
             self.cr.fatal(_(u"L'attribut 'statut' doit être parmi %s : %r"),
                           into, self.statut)
-        if self.nom == 'reuse' and self.statut != 'f':
-            self.cr.fatal(_(u"L'attribut 'statut' doit être 'f' pour reuse."))
+        if self.nom == 'reuse' and self.statut != 'c':
+            self.cr.fatal(_(u"L'attribut 'statut' doit être 'c' pour reuse."))
 
     def check_condition(self):
         """Vérifie l'attribut condition."""
