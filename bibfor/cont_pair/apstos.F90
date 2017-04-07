ce7248008a3d923445a0906207bf782825214c2d
Guillaume Drouet <guillaume.drouet@edf.fr>
2017-04-07 15:37:03 +0200

    [#25955] Up to 100 contact zone with LAC method

--- bibfor/cont_pair/apstos.F90
+++ bibfor/cont_pair/apstos.F90
@@ -122,9 +122,9 @@ implicit none
         pair_tole     = mminfr(ds_contact%sdcont_defi, 'RESI_APPA', i_zone)
         sdcont_methco = ds_contact%sdcont_defi(1:16)//'.METHCO'
         call jeveuo(sdcont_methco, 'L', vi = v_sdcont_methco)
-        if (v_sdcont_methco(zmeth*(i_zone-1)+7) .eq. 2) then 
-            pair_method  = 'ROBUSTE'        
-        else if (v_sdcont_methco(zmeth*(i_zone-1)+7) .eq. 3) then 
+        if (v_sdcont_methco(zmeth*(i_zone-1)+7) .eq. 2) then
+            pair_method  = 'ROBUSTE'
+        else if (v_sdcont_methco(zmeth*(i_zone-1)+7) .eq. 3) then
             pair_method  = 'RAPIDE'
         endif
 !
@@ -140,15 +140,15 @@ implicit none
 ! ----- Get objects
 !
         call jelira(sdappa_mast, 'LONMAX', nb_elem_mast)
-        call jelira(sdappa_slav, 'LONMAX', nb_elem_slav) 
+        call jelira(sdappa_slav, 'LONMAX', nb_elem_slav)
         call jeveuo(sdappa_mast, 'L', vi = v_sdappa_mast)
         call jeveuo(sdappa_slav, 'L', vi = v_sdappa_slav)
 !
 ! ----- Pairing
-!       
+!
 !
 ! ----- MPI initialisation
-! 
+!
         call asmpi_comm('GET', mpicou)
         call asmpi_info(mpicou,rank=i_proc , size=nb_proc)
         nb_elem_mpi  = int(nb_elem_slav/nb_proc)
@@ -156,7 +156,7 @@ implicit none
         idx_start    = 1+(i_proc)*nb_elem_mpi
         idx_end      = idx_start+nb_elem_mpi-1+nbr_elem_mpi*int((i_proc+1)/nb_proc)
         !write(*,*)"Proc : ", i_proc, "idx_start", idx_start, "idx_end", idx_end
-        nb_el_slav_mpi = idx_end - idx_start + 1 
+        nb_el_slav_mpi = idx_end - idx_start + 1
         AS_ALLOCATE(vi=v_appa_slav_mpi, size=nb_el_slav_mpi)
         v_appa_slav_mpi(:)=v_sdappa_slav(idx_start:idx_end)
         !write(*,*)"I_PROC = ", i_proc
