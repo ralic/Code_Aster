4bad912babce42b3c9e46d53cef479cffb7eecf1
ayaovi-dzifa Kudawoo <ayaovi-dzifa.kudawoo@edf.fr>
2017-04-27 08:28:06 +0200

    [#25783] partitionner (METIS, SCOTCH) on no connected cells : message to user

--- bibfor/utilitai/fetskp.F90
+++ bibfor/utilitai/fetskp.F90
@@ -1,7 +1,7 @@
 subroutine fetskp(mod,meth,nbpart)
 !
 ! ======================================================================
-! COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
+! COPYRIGHT (C) 1991 - 2017  EDF R&D                  WWW.CODE-ASTER.ORG
 ! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 ! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 ! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -127,6 +127,7 @@ subroutine fetskp(mod,meth,nbpart)
 ! ------- CREATION DE LA CONNECTIVITE DES MAILLES
 !
     call creaco(nbmato, ma, nblien)
+    if (nblien .eq. 0) call utmess('F', 'PARTITION_4')
 !
 ! ------ ON RECUPERE LES TABLEAUX CONSTRUITS DANS CREACO
 !
